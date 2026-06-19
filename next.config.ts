7cf1b4df1a2906c810ab6616dc8ee52b82b3d319
Bilal Muhammad <bilalmuhammad1240@gmail.com>
2026-06-19 22:33:37 +0200

    fix next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "**.supabase.co",
+        hostname: "kssfbnchykjgmvichbtn.supabase.co",
         pathname: "/storage/v1/object/public/**",
       },
     ],
